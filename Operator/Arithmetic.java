99bb5d8424a23b9dcd0ccbf3a1d8eba264da967b
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2023-12-30 21:07:02 +0530

    discuss about operator

---
+++ Operator/Arithmetic.java
@@ -0,0 +1,58 @@
+package Operator;
+
+public class Arithmetic {
+    public static void main(String[] args) {
+        //Arithmetic Operator
+        //1.Addition
+        
+        //initilizing variable
+        int num1=10,num2=20,sum=0;
+         
+        //Displaying num1 and num2
+
+        System.out.println("num1" + num1);
+        System.out.println("num2" + num2);
+
+        //adding of two number
+
+        sum = num1 + num2;
+        System.out.println(sum);
+
+
+        //2.Subtract
+
+        //initilizing variable
+        // int num1=10,num2=20,sum=0;
+         
+        //Displaying num1 and num2
+
+        // System.out.println("num1" + num1);
+        // System.out.println("num2" + num2);
+
+        //Subtract of two number
+
+        // sum = num1 - num2;
+        // System.out.println(sum);
+
+        //3.Multiple
+
+        //initilizing variable
+        // int num1=10,num2=20,sum=0;
+         
+        //Displaying num1 and num2
+
+        // System.out.println("num1" + num1);
+        // System.out.println("num2" + num2);
+
+        //Multiple of two number
+
+        // sum = num1 * num2;
+        // System.out.println(sum);
+
+
+
+
+
+    }
+    
+}
---
+++ Operator/Assignment.java
@@ -0,0 +1,25 @@
+package Operator;
+
+public class Assignment {
+    public static void main(String[] args) {
+        //Assignment Operator
+
+        //Decleraing variable
+
+        int num;
+        String name;
+
+        //Assign value
+
+        num =10;
+        name="Tony Stark";
+
+        //Displaying value
+
+        System.out.println("num is assigned" + num);
+        System.out.println("name is assigned" + name);
+
+        
+    }
+    
+}
