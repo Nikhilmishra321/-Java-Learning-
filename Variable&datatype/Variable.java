0b2b85d2fb37520803b12e4d3b338ac67ba42e3e
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2023-12-30 16:11:47 +0530

    there are some variable and its example

---
+++ Variable&datatype/Variable.java
@@ -0,0 +1,31 @@
+
+
+public class Variable {
+    public static void main(String[] args) {
+        
+        //Variable is a container to store of data 
+        //String
+        String name = "Hello";
+        System.out.println(name);
+
+        //Int
+        int a=10;
+        System.out.println(a);
+
+        //float
+
+        float Mynum =10.6f;
+        System.out.println(Mynum);
+
+        //boolean
+
+        boolean myBol=true;
+        System.out.println(myBol);
+
+        //String
+
+        String myText="hello";
+        System.out.println(myText);
+    }
+    
+}
