94f0b7ba53c40f19a70bbfcd9684d88961a6aa95
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2023-12-30 19:54:36 +0530

    type of variable

--- Variable&datatype/Variable.java
+++ Variable&datatype/Variable.java
@@ -26,6 +26,20 @@ public class Variable {
 
         String myText="hello";
         System.out.println(myText);
+
+        //Display Variable
+        // The println() method is often used to display variables.
+
+        // To combine both text and a variable, use the + character:
+
+        String nameString="John";
+        System.out.println("Hello" + nameString);
+
+
+         String firstName = "John ";
+         String lastName = "Doe";
+         String fullName = firstName + lastName;
+         System.out.println(fullName);
     }
     
 }
