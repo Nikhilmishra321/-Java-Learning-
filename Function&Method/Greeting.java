97350be1dddf4293ef577c8350de700c3802fd52
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-01-11 12:58:16 +0530

    greeting message print

---
+++ Function&Method/Greeting.java
@@ -0,0 +1,12 @@
+
+public class Greeting {
+    public static void main(String[] args) {
+        greeting();
+        greeting();
+        
+    }
+    static void greeting(){
+        System.out.println("Hello world");
+
+    }
+}
