35995f669d4f471b6e5884d43634196f5e31dcf1
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-01-12 09:04:32 +0530

    swap of two number

---
+++ Function&Method/functionPractise/ProductOfNumber.java
@@ -0,0 +1,17 @@
+public class ProductOfNumber {
+    public static void main(String[] args) {
+        int a =10;
+        int b =20;
+        int prod=multiply(a, b);
+        System.out.println("multiply of two number" + prod);
+        
+    }
+
+    public static int multiply(int a, int b){
+        int product=a * b;
+        return product;
+
+    }
+
+    
+}
--- Function&Method/functionPractise/Swapping.java
+++ Function&Method/functionPractise/Swapping.java
@@ -2,10 +2,24 @@ package functionPractise;
 
 public class Swapping {
     public static void main(String[] args) {
+        //swap value exchanged
+        int a=10;
+        int b=20;
+
+
+        //swap
+        int temp= a;
+         a=b;
+         b= temp;
+
+        System.out.println(a);
+        System.out.println(b);
+
         
     }
-    public static void Swap(){
+    // public static void Swap(){
         
-    }
+
+    // }
     
 }
