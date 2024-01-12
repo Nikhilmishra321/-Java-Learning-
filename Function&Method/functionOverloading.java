40df19839275c75f3fa6e25e76c338fe5fb21ad7
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-01-12 10:44:00 +0530

    function overloading

--- Function&Method/functionOverloading.java
+++ Function&Method/functionOverloading.java
@@ -1,7 +1,11 @@
+
+
 public class functionOverloading {
     public static void main(String[] args) {
-        System.out.println(sum(10, 3));
-        System.out.println(sum(10, 2, 1));
+        // System.out.println(sum(10, 3));
+        // System.out.println(sum(10.5f, 2.1f));
+
+        isPrime(5);
         
     }
     //sum of two numbers
@@ -10,8 +14,26 @@ public class functionOverloading {
 
     }
     //sum of three numbers
-    public static int sum(int a, int b, int c){
-        return a+b+c;
+    // public static int sum(int a, int b, int c){
+    //     return a+b+c;
+    // }
+    //func to calculate int sum
+
+    public static float sum(float a,float b ){
+        return a +b;
     }
+    public static boolean isPrime(int n){
+        boolean isPrime = true;
+        for(int i=2; i<=n-1; i++){
+            if(n %  i==0){
+                isPrime=false;
+
+            }
+        }
+        return isPrime;
+        
+    }
+    
+
     
 }
