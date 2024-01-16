1260fa1809aa9ccc12b3bd3297f4225e9f59d379
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-01-16 21:07:56 +0530

    question  practise

---
+++ Week2_Prac-Ses1/Solution5.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Solution5 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int year =sc.nextInt();
+        if(year%4 ==0){
+            if(year%100==0){
+                if(year%400==0){
+                    System.out.println("Leap year");
+                }
+                else
+                {
+                    System.out.println("Not a leap year");
+                }
+            }
+            else{
+                System.out.println("leap year");
+            }
+        }
+    }
+    
+}
