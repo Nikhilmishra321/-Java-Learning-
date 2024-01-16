c52b2c8889cd7e663ea0f17d3be0c4edb04c09ff
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-01-16 09:49:38 +0530

    for each loop using print arrays element

---
+++ ArraysBasic/A.java
@@ -0,0 +1,16 @@
+package ArraysBasic;
+
+public class A {
+    public static void main(String[] args) {
+        int a[] =new int[3];
+        a[0]=10;
+        a[1]=20;
+        a[2]=30;
+
+        for (int i : a) {
+            System.out.println(i+"");
+            
+        }
+    }
+    
+}
