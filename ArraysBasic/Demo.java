2f334f79ff6279ae7387a506e80af72ed0eba728
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-01-16 09:41:18 +0530

    static arrays

---
+++ ArraysBasic/Demo.java
@@ -0,0 +1,20 @@
+package ArraysBasic;
+
+public class Demo {
+    public static void main(String[] args) {
+        // int a[] = {3,4,6,8,5};
+        // System.out.println(a[3]);
+
+        //static arrays
+
+        int a[]= new int[3];
+        a[0]=10;
+        a[1]=20;
+        a[2]=30;
+        for(int i=0 ; i<3 ; i++){
+            System.out.println(a[i]);
+        }
+
+
+    }
+}
