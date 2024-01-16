b8f5a6cc8995339b645d5b585267b71de0316374
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-01-16 10:01:58 +0530

    arrays method used to print arrays element

---
+++ ArraysBasic/B.java
@@ -0,0 +1,15 @@
+package ArraysBasic;
+//to string() asList() deepToString()
+
+import java.util.Arrays;
+
+public class B {
+    public static void main(String[] args) {
+        String a[] ={"coding" ,"challenge" ,"hunderdays"};
+
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.asList(a));
+        
+    }
+    
+}
