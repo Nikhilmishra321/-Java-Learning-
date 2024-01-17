bbeed6d2181841feff2fea1a7a5012c747caf0c0
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-01-17 10:53:39 +0530

    how many times hello print

---
+++ Week2_Prac-Ses2/Solution1.java
@@ -0,0 +1,12 @@
+//how many times hello print
+public class Solution1 {
+    public static void main(String[] args) {
+        for (int i=0; i<5; i++){
+            System.out.println("Hello");
+
+            i+=2;
+
+        }
+    }
+    
+}
