424bbd05b454ff31c20d6f51123262991a22a31b
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-01-19 19:59:01 +0530

    marks of arrays

---
+++ ARRAYS(Part_1)/marks.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class marks {
+    public static void main(String[] args) {
+        int Marks[] =new int [50];
+
+        Scanner sc= new Scanner(System.in);
+        Marks[0]=sc.nextInt();
+        Marks[1]=sc.nextInt();
+        Marks[2]=sc.nextInt();
+
+        System.out.println("phys:" + Marks[0] , );
+
+    }
+    
+}
