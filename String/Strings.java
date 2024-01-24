ac5f23b0ca982aaa73c9da2a9839a9f7247b0520
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-01-24 19:48:50 +0530

    length of strings

--- String/Strings.java
+++ String/Strings.java
@@ -4,14 +4,17 @@ import java.util.Scanner;
 
 public class Strings {
     public static void main(String[] args) {
-        char arr[]={'a' ,'b','c','d','e'};
-        String str="abcd";
-        String str2=new String("xyz");
+        // char arr[]={'a' ,'b','c','d','e'};
+        // String str="abcd";
+        // String str2=new String("xyz");
 
         //string are immutable
-        Scanner sc= new Scanner(System.in);
-         String name =sc.next();
-         System.out.println(name);
+        // Scanner sc= new Scanner(System.in);
+        //  String name =sc.next();
+        //  System.out.println(name);
+
+        String fullname="TonyStark";
+        System.out.println(fullname.length());
 
     }
     
