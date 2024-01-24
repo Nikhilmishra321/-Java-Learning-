76afe38931f7f453391d2ec5df183fca8a5271d3
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-01-24 19:41:41 +0530

    strings

---
+++ String/Strings.java
@@ -0,0 +1,14 @@
+package String;
+
+public class Strings {
+    public static void main(String[] args) {
+        char arr[]={'a' ,'b','c','d','e'};
+        String str="abcd";
+        String str2=new String("xyz");
+
+        //string are immutable
+        
+
+    }
+    
+}
