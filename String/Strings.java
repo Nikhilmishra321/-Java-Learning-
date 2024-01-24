5e7a4b9c9d1d3a3367cb8e164c47e3613ddd5309
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-01-24 19:46:01 +0530

    Input/Output

--- String/Strings.java
+++ String/Strings.java
@@ -1,5 +1,7 @@
 package String;
 
+import java.util.Scanner;
+
 public class Strings {
     public static void main(String[] args) {
         char arr[]={'a' ,'b','c','d','e'};
@@ -7,7 +9,9 @@ public class Strings {
         String str2=new String("xyz");
 
         //string are immutable
-        
+        Scanner sc= new Scanner(System.in);
+         String name =sc.next();
+         System.out.println(name);
 
     }
     
