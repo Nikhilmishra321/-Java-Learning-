b3ff9edd3b5a20122d31daa134512ede0bf706b2
Nikhil Mishra <140895857+Nikhilmishra321@users.noreply.github.com>
2024-01-24 20:28:18 +0530

    Pallidrome in java

---
+++ String/Pallidrome.java
@@ -0,0 +1,21 @@
+package String;
+
+public class Pallidrome {
+    public static boolean isPallidrome(String str){
+        for(int i=0; i<str.length()/2; i++){
+            int n= str.length();
+            if(str.charAt(i) !=str.charAt(n-1-i)){
+                return false;
+
+            }
+        }
+        return true;
+
+    }
+    public static void main(String[] args) {
+        String str="racecar";
+        System.out.println(isPallidrome(str));
+        
+    }
+    
+}
